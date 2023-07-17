1b2c1a54872fb2a19535ef79356b6bc43e550b2b
Andreia Santos <137791508+AndreiaCa@users.noreply.github.com>
2023-07-17 16:36:02 -0300

    Configurando método conectar e desconectar bd

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -1,6 +1,7 @@
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
@@ -17,18 +18,26 @@ import javax.swing.JOptionPane;
  */
 public class conectaDAO {
     
+    Connection conn = null;
     public Connection connectDB(){
-        Connection conn = null;
+       
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11","root","imadeveloper");
+            System.out.println("Conexao concluída ");
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
     }
     
-    
+    public void desconectar() {
+        try {
+            conn.close();
+            System.out.println("Conexao com o banco de dados fechada");
+        } catch (SQLException ex) {
+
+        }
+    }
 }
